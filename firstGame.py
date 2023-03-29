8bf314208a70c61a8a4e68464fdb372df6cf3323
Kethankrk <kethankrk@gmail.com>
2023-03-30 01:19:49 +0530

    fixed the shadow

--- firstGame.py
+++ firstGame.py
@@ -2,51 +2,75 @@ import pygame
 
 pygame.init()
 
-screen = pygame.display.set_mode((800, 600))
+height = 608
+width = 1200
+screen = pygame.display.set_mode((width, height))
 
 pygame.display.set_caption("First Python Game")
 
 goku_image = pygame.image.load("goku.png")
-playerX = 300
-playerY = 300
-
-changeX = 0
-changeY = 0
 
 
-def player(x, y):
-    screen.blit(goku_image, (x, y))
+
+class Player:
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+        self.changeX = 0
+        self.changeY = 0
+        self.left = False
+        self.right = False
+        self.up = False
+        self.down = False
+        self.speed = 2
+
+    def Start(self):
+        screen.blit(goku_image, (self.x, self.y))
+
+    def Render(self):
+        if self.left and self.x >= 0:
+            self.x += -self.speed
+        if self.right and self.x <= width - 110:
+            self.x += self.speed
+        if self.up and self.y >= 0:
+            self.y += -self.speed
+        if self.down and self.y <= height - 130:
+            self.y += self.speed
+        
+        self.Start()
 
 
 running = True
-
+new = Player(300, 200)
 while running:
+
+    screen.fill((0,0,0))
+    # new.Start()
+
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
 
         if event.type == pygame.KEYDOWN:
-            if event.key == pygame.K_LEFT:
-                changeX = - 0.4
-            if event.key == pygame.K_RIGHT:
-                changeX = 0.4
+            if event.key == pygame.K_a:
+                new.left = True
+            if event.key == pygame.K_d:
+                new.right = True
+            if event.key == pygame.K_w:
+                new.up = True
+            if event.key == pygame.K_s:
+                new.down = True
 
-            if event.key == pygame.K_UP:
-                changeY = - 0.4
-            if event.key == pygame.K_DOWN:
-                changeY =  0.4
         if event.type == pygame.KEYUP:
-            if event.key == pygame.K_LEFT or event.key == pygame.K_RIGHT:
-                changeX = 0
-            if event.key == pygame.K_UP or event.key == pygame.K_DOWN:
-                changeY = 0
-
-    playerY += changeY
-    playerX += changeX
-    if playerY > 600 or playerY < 0:
-        continue
-    if playerX > 720 or playerX < 0:
-        continue
-    player(playerX, playerY)
+            if event.key == pygame.K_a:
+                new.left = False
+            if event.key == pygame.K_d:
+                new.right = False
+            if event.key == pygame.K_w:
+                new.up = False
+            if event.key == pygame.K_s:
+                new.down = False
+    # new.Start()
+    new.Render()
 
     pygame.display.update()
